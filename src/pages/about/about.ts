f0f2848a9c4557d00857479b91911ce8624d3bad
Jesus Guerrero <rocha.jesus@hotmail.com>
2016-12-17 15:06:12 -0800

    Patching Usage

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -4,7 +4,8 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
-  templateUrl: 'about.html'
+  templateUrl: 'about.html',
+  styleUrls: ['assets/bower_components/bootstrap/dist/css/bootstrap.css']
 })
 export class AboutPage {
 
